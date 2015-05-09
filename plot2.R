a7df9ce2b284bf30fab790ed305b754cebfd18ef
martjushev <martjushev@gmail.com>
2015-05-09 17:18:28 +0300

    plot2

---
+++ plot2.R
@@ -0,0 +1,7 @@
+csv<-read.csv('household_power_consumption.txt', sep = ';', na.strings='?')
+csv<-csv[csv$Date == '1/2/2007' | csv$Date == '2/2/2007',]
+csv_clean<-csv[,2:9]
+csv_clean[,1]<-as.POSIXct(strptime(paste(csv[,1],csv[,2]), format='%d/%m/%Y %H:%M:%S'))
+plot(csv_clean$Time, csv_clean$Global_active_power, type = 'l', xlab = '', ylab = 'Global Active Power (kilowatts)')
+dev.copy(png, file = "plot2.png")
+dev.off()
